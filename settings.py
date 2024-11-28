69e4fbd79a9e5b86ea87dbad0083dc15bcdad1ee
phbruemmer <phbruemmer@gmail.com>
2024-11-28 11:14:07 +0100

    - added settings.py for constants - added database_connector.py to establish connection to the database (currently only SQL in work)

---
+++ settings.py
@@ -0,0 +1,8 @@
+"""
+In settings.py you can change IPs (e.g. Database / etc.)
+- Add multiple IPs to the (connection_routing) dictionary, make sure you don't overwrite previously initialized data.
+"""
+
+connection_routing = {
+    'database': ('192.168.115.200', 1443),
+}
