263f57fd27a6561bf35a655ece9369542fa00b13
phbruemmer <phbruemmer@gmail.com>
2024-11-29 23:26:59 +0100

    - Tested MySQL connection with mysql.connector library

--- backend/database_connector.py
+++ backend/database_connector.py
@@ -1,3 +1,31 @@
-import socket
-import threading
+import mysql.connector
+import logging
 
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+
+CONFIG = {
+    'host': 'localhost',
+    'user': 'root',
+    'password': '',
+    'database': 'test',
+}
+
+try:
+    connection = mysql.connector.connect(**CONFIG)
+
+    if connection.is_connected():
+        logging.info("[database_connector-test] Successfully connected to the database.")
+        # DB Tasks here
+except mysql.connector.Error as e:
+    logging.error('[database_connector-test] Connection error: %s', e)
+except Exception as e:
+    logging.error('[database_connector-test] Unexpected error: %s', e)
+finally:
+    if connection.is_connected():
+        connection.close()
+        logging.info("[database_connector-test] Database connection closed.")
