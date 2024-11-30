77c0b085f59a0e279664e7931041cbfbe1dbea5f
phbruemmer <phbruemmer@gmail.com>
2024-11-30 22:08:51 +0100

    - Small changes in the database_connector.py file for better DB handling in the future

--- backend/database_connector.py
+++ backend/database_connector.py
@@ -1,6 +1,10 @@
 import mysql.connector
 import logging
 
+# !NOT IMPORTANT FOR THIS FILE!
+# The locals() function returns a dictionary containing all the variables and their values that are
+# declared in the current scope.
+# !NOT IMPORTANT FOR THIS FILE!
 
 logging.basicConfig(
     level=logging.INFO,
@@ -15,17 +19,29 @@ CONFIG = {
     'database': 'test',
 }
 
-try:
-    connection = mysql.connector.connect(**CONFIG)
 
-    if connection.is_connected():
-        logging.info("[database_connector-test] Successfully connected to the database.")
-        # DB Tasks here
-except mysql.connector.Error as e:
-    logging.error('[database_connector-test] Connection error: %s', e)
-except Exception as e:
-    logging.error('[database_connector-test] Unexpected error: %s', e)
-finally:
-    if connection.is_connected():
-        connection.close()
-        logging.info("[database_connector-test] Database connection closed.")
+class Database:
+    def connect(self, function):
+        """
+        Connects to the Database
+        :param function: Function to execute database tasks.
+        :return:
+        """
+        try:
+            with mysql.connector.connect(**CONFIG) as conn:
+                if conn.is_connected():
+                    logging.info("[database_connector-test] Successfully connected to the database.")
+                    with conn.cursor() as cursor:
+                        function(cursor)
+        except mysql.connector.Error as e:
+            logging.error('[database_connector-test] Connection error: %s', e)
+            raise
+        except Exception as e:
+            logging.error('[database_connector-test] Unexpected error: %s', e)
+            raise e
+
+    def read(self):
+        pass
+
+    def write(self):
+        pass
