d67857b4582473dfc42ddebeff769f6dbf344f6f
phbruemmer <phbruemmer@gmail.com>
2024-12-01 19:59:23 +0100

    - Added connect -decorators to read and write functions - removed Database class for better decorator handling

--- backend/database_connector.py
+++ backend/database_connector.py
@@ -20,28 +20,56 @@ CONFIG = {
 }
 
 
-class Database:
-    def connect(self, function):
-        """
-        Connects to the Database
-        :param function: Function to execute database tasks.
-        :return:
-        """
+def connect(function):
+    """
+    Decorator to manage database connection and cursor setup.
+    :param function: The function to execute with a database connection and cursor.
+    :return:
+    """
+
+    def wrapper(*args, **kwargs):
+        conn = None
+        cursor = None
         try:
-            with mysql.connector.connect(**CONFIG) as conn:
-                if conn.is_connected():
-                    logging.info("[database_connector-test] Successfully connected to the database.")
-                    with conn.cursor() as cursor:
-                        return function(cursor)
+            conn = mysql.connector.connect(**CONFIG)
+            if conn.is_connected():
+                logging.info("[database_connector-test] Successfully connected to the database.")
+                cursor = conn.cursor()
+                return function(cursor, conn)
         except mysql.connector.Error as e:
             logging.error('[database_connector-test] Connection error: %s', e)
             raise
         except Exception as e:
             logging.error('[database_connector-test] Unexpected error: %s', e)
             raise e
+        finally:
+            if cursor:
+                cursor.close()
+                logging.info("[wrapper] cursor closed.")
+            if conn and conn.is_connected():
+                conn.close()
+                logging.info("[wrapper] connection closed.")
 
-    def read(self):
-        pass
+    return wrapper
 
-    def write(self):
-        pass
+
+@connect
+def read(cursor, conn):
+    cursor.execute("SELECT * FROM users")
+    rows = cursor.fetchall()
+    for row in rows:
+        print(f"[read] {row}")
+    return rows
+
+
+@connect
+def write(cursor, conn):
+    cursor.execute("INSERT INTO users (username, email, password) VALUES (%s, %s, %s)",
+                   ("Test", "test@test.test", "test"))
+    conn.commit()
+    logging.info("[database_connector - write] Inserted data into the database.")
+
+
+if __name__ == "__main__":
+    # write()
+    read()
