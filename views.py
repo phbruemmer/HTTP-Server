d721152087c12b3134cfb74839e4ada2820682c6
phbruemmer <phbruemmer@gmail.com>
2024-12-03 01:25:24 +0100

    - Added database to register and login view - Users can now create accounts and login with their accounts

--- views.py
+++ views.py
@@ -1,27 +1,24 @@
-from backend import render, request_handler
+from backend import render, request_handler, database_connector as dc
+import hashlib
 
 
 def main(request):
     args = {
-        'var_1': '',
-        'var_2': '',
         'var_3': 'Welcome!',
         'info': ''
     }
 
     if request['method'] == 'POST':
         params = request_handler.get(request['body'])
-        args = {
-            'var_1': params['username'],
-            'var_2': params['password']
-        }
-        if args['var_1'] == 'robert' and args['var_2'] == '1234':
-            args['var_3'] = 'Success!'
-            args['info'] = 'Your login was successful!'
+        user_id = dc.get_id('users', 'username', params['username'], force_clean=True)
+        hash_object = hashlib.sha256()
+        hash_object.update(params['password'].encode())
+        hash_hex = hash_object.hexdigest()
+        if user_id and dc.get_by_id('users', user_id)[3] == hash_hex:
             return render.render(request, 'files/home.html', args)
         else:
             args['var_3'] = 'Welcome!'
-            args['info'] = 'Wrong credentials! Try username <b>robert</b> and password <b>1234</b>'
+            args['info'] = '<b>Wrong credentials!</b>'
     return render.render(request, 'files/index.html', args)
 
 
@@ -38,6 +35,11 @@ def register(request):
         if not params['password-0'] == params['password-1']:
             args['info'] = 'Password does not match!'
         else:
+            hash_object = hashlib.sha256()
+            hash_object.update(params['password-0'].encode())
+            hash_hex = hash_object.hexdigest()
+            code = dc.write('users', (params['username'], params['email'], hash_hex))
+            print(f"[register] database exit code {code}")
             return render.render(request, 'files/home.html', args)
 
     return render.render(request, 'files/register.html', args)
