a1a5566aad87514e11aa5abdbde36af8c69765b7
phbruemmer <phbruemmer@gmail.com>
2024-12-06 16:07:41 +0100

    - changed parameters to **kwargs for better readability

--- DEFAULTS.py
+++ DEFAULTS.py
@@ -13,21 +13,28 @@ CODES = {
     200: '200 OK',
     404: '404 Not Found',
     500: '500 Internal Server Error',
+    301: '301 Moved Permanently',
     302: '302 Found'
 }
 
 
-def generate_response(code, server, full_path, close_connection, location=None, modified_file=None):
+def generate_response(code, **kwargs):
     """
     Generate an HTTP response based on the status code, server name, and requested file path.
+    :param code: The HTTP status Code (e.g. 200, 400).
+    :param full_path: The full path to the file
     :param location: URL to redirect to.
     :param modified_file: In case the file was internally modified you can add new file content in here.
     :param close_connection: True: close || False: keep-alive
-    :param code: The HTTP status Code (e.g. 200, 400).
     :param server: The server name or identifier
-    :param full_path: The full path to the file
     :return: A string representing the complete HTTP response
     """
+    server = kwargs.get('server')
+    full_path = kwargs.get('full_path')
+    location = kwargs.get('location')
+    modified_file = kwargs.get('modified_file')
+    close_connection = kwargs.get('close_connection')
+
     current_time = datetime.now()
     formatted_time = current_time.strftime("%a, %d %b %Y %H:%M:%S GMT")
 
@@ -49,7 +56,7 @@ def generate_response(code, server, full_path, close_connection, location=None, 
     http_location = ""
 
     if location:
-        http_location += location + "\r\n"
+        http_location += f"Location: {location}\r\n"
 
     headers = (f"HTTP/1.1 {CODES[code]}\r\n"
                f"Date: {formatted_time}\r\n"
