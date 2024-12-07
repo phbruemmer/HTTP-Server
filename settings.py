d9cfead70d58f56f5ced52b30d01417782c5a929
phbruemmer <phbruemmer@gmail.com>
2024-12-07 22:45:49 +0100

    - Added DEFAULT_PATHS for better readability and modularity in the codebase

--- settings.py
+++ settings.py
@@ -2,9 +2,17 @@
 In settings.py you can change IPs (e.g. Database / etc.)
 - Add multiple IPs to the (connection_routing) dictionary, make sure you don't overwrite previously initialized data.
 """
+import os
 
 connection_routing = {
     'database': 'localhost',
 }
 
 allowed_tables = ['users']
+
+DEFAULT_PATH = "files/"
+
+DEFAULT_PATHS = {
+    '404': os.path.join(DEFAULT_PATH, '404.html'),
+    '500': os.path.join(DEFAULT_PATH, '500.html')
+}
