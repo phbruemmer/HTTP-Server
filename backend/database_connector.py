f4b09c2de18da032854be96e4a94c2355f7d78fd
phbruemmer <phbruemmer@gmail.com>
2024-12-07 23:45:40 +0100

    - simplified the functions

--- backend/database_connector.py
+++ backend/database_connector.py
@@ -36,7 +36,7 @@ def connect(function):
             if conn.is_connected():
                 logging.info("[database_connector-test] Successfully connected to the database.")
                 cursor = conn.cursor()
-                return function(cursor, conn, *args, **kwargs)
+                return function(cursor=cursor, conn=conn, *args, **kwargs)
         except mysql.connector.Error as e:
             logging.error('[database_connector-test] Connection error: %s', e)
             raise
@@ -54,16 +54,16 @@ def connect(function):
 
 
 @connect
-def validate(cursor, conn, table, value, position):
+def validate(table, value, position, **kwargs):
     """
     Function to validate values in the database and check for duplicates.
-    :param cursor: -
-    :param conn: -
     :param table: Name of the table (string)
     :param value: Value to look for in the db
     :param position: column position (eg. id / username / email / password -> 0 / 1 / 2 / 3)
     :return: True if item exists in the database.
     """
+    cursor = kwargs.get('cursor')
+
     valid_value = False
     cursor.execute(f"SELECT * FROM {table}")
     rows = cursor.fetchall()
@@ -74,18 +74,18 @@ def validate(cursor, conn, table, value, position):
 
 
 @connect
-def get_id(cursor, conn, table, column, value, cleaned=False, force_clean=False):
+def get_id(table, column, value, cleaned=False, force_clean=False, **kwargs):
     """
     gets the id of a table entry in the database.
-    :param force_clean: returns first element
-    :param cleaned: cleans the output when set to true
-    :param cursor: -
-    :param conn: -
+    :param force_clean: returns first element (no list)
+    :param cleaned: cleans the output when set to true (list)
     :param table: table name (string)
     :param column: column name (string)
     :param value: Value to look for
     :return: id (integer)
     """
+    cursor = kwargs.get('cursor')
+
     query = f"SELECT id FROM {table} WHERE {column} = %s"
     cursor.execute(query, (value,))
 
@@ -106,14 +106,14 @@ def get_id(cursor, conn, table, column, value, cleaned=False, force_clean=False)
 
 
 @connect
-def get_by_id(cursor, conn, table, entry_id):
+def get_by_id(table, entry_id, **kwargs):
     """
-    :param cursor: -
-    :param conn: -
     :param table: name of the table (string)
     :param entry_id: id (integer)
     :return: full entry from id
     """
+    cursor = kwargs.get('cursor')
+
     query = f"SELECT * FROM {table} WHERE id = %s"
     cursor.execute(query, (entry_id,))
     result = cursor.fetchone()
@@ -126,15 +126,16 @@ def get_by_id(cursor, conn, table, entry_id):
 
 
 @connect
-def remove(cursor, conn, table, id_value):
+def remove(table, id_value, **kwargs):
     """
     Removes data from specific table with specific item ID ( '*' for all items)
-    :param cursor: -
-    :param conn: -
     :param table: table name in the database
     :param id_value: item id
     :return:
     """
+    cursor = kwargs.get('cursor')
+    conn = kwargs.get('conn')
+
     done = False
     if table not in settings.allowed_tables:
         logging.error("[remove] Could not remove the table / items - Not in allowed list in settings.py")
@@ -162,15 +163,15 @@ def remove(cursor, conn, table, id_value):
 
 
 @connect
-def read(cursor, conn, table, printable=False):
+def read(table, printable=False, **kwargs):
     """
     Reads the users database table.
     :param table: table name (string)
     :param printable: prints out the rows
-    :param cursor: -
-    :param conn: -
     :return: rows
     """
+    cursor = kwargs.get('cursor')
+
     cursor.execute(f"SELECT * FROM {table}")
     rows = cursor.fetchall()
     if printable:
@@ -180,26 +181,19 @@ def read(cursor, conn, table, printable=False):
 
 
 @connect
-def write(cursor, conn, table, value):
+def write(table, value, **kwargs):
     """
     Writes data into the users database table
     :param table: table name in the database (string)
     :param value: Value to add to the database (tuple)
-    :param cursor: -
-    :param conn: -
     :return:
     """
-    pos = 1
-    for val in value:
-        if validate(table, val, pos):
-            logging.error("[write] Could not write data without creating duplicates.")
-            return False
-        pos += 1
+    cursor = kwargs.get('cursor')
+    conn = kwargs.get('conn')
 
     cursor.execute(f"INSERT INTO {table} (username, email, password) VALUES (%s, %s, %s)", value)
     conn.commit()
     logging.info("[database_connector - write] Inserted data into the database.")
-    return True
 
 
 if __name__ == "__main__":
