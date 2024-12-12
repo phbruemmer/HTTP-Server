194d88f544f1234cca40eef837c3fff8aa8c60ec
phbruemmer <phbruemmer@gmail.com>
2024-12-12 22:51:26 +0100

    - Added add app command to manage.py to add multiple apps to the application

--- manage.py
+++ manage.py
@@ -7,38 +7,51 @@ import logging
 import sys
 
 
-def update(args):
-    match args[2]:
-        case 'statics':
-            STATIC = "backend/STATIC_FILES"
-            if not os.path.exists(settings.DEFAULT_STATIC_FILE_PATH) or not os.path.exists(STATIC):
-                logging.error("[update] No such path found.")
-                raise
+def update_statics():
+    STATIC = "backend/STATIC_FILES"
+    if not os.path.exists(settings.DEFAULT_STATIC_FILE_PATH) or not os.path.exists(STATIC):
+        logging.error("[update] No such path found.")
+        raise
+    try:
+        for file in os.listdir(STATIC):
+            file_path = os.path.join(STATIC, file)
             try:
-                for file in os.listdir(STATIC):
-                    file_path = os.path.join(STATIC, file)
-                    try:
-                        if os.path.isfile(file_path) or os.path.islink(file_path):
-                            os.unlink(file_path)
-                        elif os.path.isdir(file_path):
-                            shutil.rmtree(file_path)
-                    except Exception:
-                        logging.error("[update] Unexpected error while removing static files.")
+                if os.path.isfile(file_path) or os.path.islink(file_path):
+                    os.unlink(file_path)
+                elif os.path.isdir(file_path):
+                    shutil.rmtree(file_path)
+            except Exception:
+                logging.error("[update] Unexpected error while removing static files.")
 
-                for item in os.listdir(settings.DEFAULT_STATIC_FILE_PATH):
-                    source_item = os.path.join(settings.DEFAULT_STATIC_FILE_PATH, item)
-                    destination_item = os.path.join(STATIC, item)
-                    if os.path.isdir(source_item):
-                        shutil.copytree(source_item, destination_item)
-                    else:
-                        shutil.copy2(source_item, destination_item)
-                logging.info("[update] successfully updated static file system.")
-            except FileNotFoundError as e:
-                logging.error("[update] Could not find file: %s", e)
-                raise
-            except Exception as e:
-                logging.error("[update] Unkown error: %s", e)
-                raise
+        for item in os.listdir(settings.DEFAULT_STATIC_FILE_PATH):
+            source_item = os.path.join(settings.DEFAULT_STATIC_FILE_PATH, item)
+            destination_item = os.path.join(STATIC, item)
+            if os.path.isdir(source_item):
+                shutil.copytree(source_item, destination_item)
+            else:
+                shutil.copy2(source_item, destination_item)
+        logging.info("[update] successfully updated static file system.")
+    except FileNotFoundError as e:
+        logging.error("[update] Could not find file: %s", e)
+        raise
+    except Exception as e:
+        logging.error("[update] Unkown error: %s", e)
+        raise
+
+
+def add_app(name):
+    if os.path.exists(name):
+        logging.error("[add] Can not overwrite existing apps.")
+        return
+    #                                         #
+    # COPY VIEW FILES TO THE CORRECT LOCATION #
+    #                                         #
+
+
+def update(arguments):
+    match arguments[2]:
+        case 'statics':
+           update_statics()
 
 
 def runserver(arguments):
@@ -47,6 +60,17 @@ def runserver(arguments):
     if len(arguments) == 3:
         HOST, PORT = arguments[2].split(':')
     http_server.Server(HOST, int(PORT)).start_server()
+
+
+def add(arguments):
+    match arguments[2]:
+        case 'app':
+            if not len(arguments) == 3:
+                logging.error("[add] Can not add an app without a name.")
+                return
+            add_app(arguments[3])
+
+
 
 
 def main():
