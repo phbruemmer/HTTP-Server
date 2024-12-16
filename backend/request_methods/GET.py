8aded2964458d0936fda50dcb6db3bfc9599dc94
phbruemmer <phbruemmer@gmail.com>
2024-12-16 15:23:57 +0100

    - Moved Get function into a separate directory

---
+++ backend/request_methods/GET.py
@@ -0,0 +1,22 @@
+from backend import url_handler, error_handling
+import logging
+
+
+def GET(HOST, request):
+    """
+    Handles GET method from the HTTP request.
+    :param HOST: Host
+    :param request: request Hashmap with HTTP information
+    :return:
+    """
+    if 'html' in request['headers']['Accept']:
+        logging.info("[GET] Path found - 200 OK")
+        response = url_handler.handle(request)
+    elif 'css' in request['headers']['Accept'] and url_handler.get_statics(request) is not None:
+        logging.info("[GET] adding css - 200 OK")
+        path = url_handler.get_statics(request)  # For safe file handling
+        response = url_handler.handle_statics(HOST, path)
+    else:
+        logging.info("[GET] No such path found - 404 Not Found.")
+        response = error_handling.render_error(HOST, 404)
+    return response
