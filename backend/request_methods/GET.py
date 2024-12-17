9daa272653a126a219184500ffab46e0fa6b2beb
phbruemmer <phbruemmer@gmail.com>
2024-12-17 22:05:10 +0100

    - changed css handling for more readable and efficient code instead of executing the function twice.

--- backend/request_methods/GET.py
+++ backend/request_methods/GET.py
@@ -9,13 +9,17 @@ def GET(HOST, request):
     :param request: request Hashmap with HTTP information
     :return:
     """
+
     if 'html' in request['headers']['Accept']:
         logging.info("[GET] Path found - 200 OK")
         response = url_handler.handle(request)
-    elif 'css' in request['headers']['Accept'] and url_handler.get_statics(request) is not None:
+    elif 'css' in request['headers']['Accept']:
         logging.info("[GET] adding css - 200 OK")
         path = url_handler.get_statics(request)  # For safe file handling
-        response = url_handler.handle_statics(HOST, path)
+        if path is not None:
+            response = url_handler.handle_statics(HOST, path)
+        else:
+            response = error_handling.render_error(HOST, 404)
     else:
         logging.info("[GET] No such path found - 404 Not Found.")
         response = error_handling.render_error(HOST, 404)
