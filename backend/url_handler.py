310eaef51cd8a5cf05309be8d94303ef067bd597
phbruemmer <phbruemmer@gmail.com>
2024-12-17 22:07:21 +0100

    - Added a path cleaner to url_handler.py

--- backend/url_handler.py
+++ backend/url_handler.py
@@ -1,15 +1,26 @@
 from backend import DEFAULTS, router
 import logging
+import settings
 import os.path
 import urls
+
+
+def clean_path(function):
+    def wrapper(request):
+        requested_path = request['path'].lstrip('/')
+        requested_path = requested_path.split(settings.DEFAULT_STATIC_FILE_PATH)
+        requested_path = requested_path[1].replace("'", "").replace('"', '')
+        file_path = os.path.join(DEFAULTS.STATIC, requested_path)
+        return function(file_path)
+    return wrapper
 
 
 def handle(request):
     return router.Router(urls.URL_PATTERNS).resolve(request['path'], request)
 
 
-def get_statics(request):
-    file_path = os.path.join(DEFAULTS.STATIC, request['path'].lstrip('/'))
+@clean_path
+def get_statics(file_path):
     try:
         if os.path.abspath(file_path).startswith(os.path.abspath(DEFAULTS.STATIC)) and os.path.isfile(file_path):
             return file_path
@@ -27,4 +38,3 @@ def handle_statics(host, path):
                                           file_content=file_content,
                                           content_type=content_type)
     return response
-
