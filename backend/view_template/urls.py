219a536fe2128fa16c53ae57cf3f44e15211fa8c
phbruemmer <phbruemmer@gmail.com>
2024-12-18 19:56:49 +0100

    - Added commenting to urls.py and views.py template.

---
+++ backend/view_template/urls.py
@@ -0,0 +1,15 @@
+from backend.router import path, include
+
+"""
+############################################################
+
+Add routing:
+ -> single path:            path('/path', view-function)
+ -> include app urls:       path('/path', include(app))
+
+############################################################
+"""
+
+URL_PATTERNS = [
+
+]
---
+++ backend/view_template/views.py
@@ -0,0 +1,14 @@
+from backend import render
+
+"""
+Example view:
+
+def home(request):                      # view function
+    args = {
+        'username': 'test',             # arguments dictionary for the HTML file
+    }
+                                        # returns an HTTP response with the prepared HTML file data
+    return render.render(request, 'main/HTML_files/home.html', args) 
+"""
+
+# Create your views here
